ca20da439f6082ab17c85240caaa81987ad171c7
HayataSama <amir.khateri135@gmail.com>
2024-01-26 22:31:09 +0330

    colored output

--- maze/main.py
+++ maze/main.py
@@ -1,5 +1,6 @@
 import random
 
+from colors import colors
 from mazebackend import generate_maze
 
 # get maze width and maze height from the user
@@ -13,3 +14,15 @@ assert HEIGHT % 2 == 1 and HEIGHT >= 3
 random.seed(int(random.random() * 10))
 maze = generate_maze(HEIGHT, WIDTH)  # generated maze is a numpy ndarray
 print(maze)
+
+
+def printc(*text, color="\033[0m"):
+    """
+    ### Summary:
+        prints colored text to the terminal using ANSI Codes and python's built-in print() function
+
+    ### Args:
+        text: the text to be printed (accepts multiple values).
+        color (str, optional): use colors.fg._color_ to select the text color. Defaults to white.
+    """
+    print(*[f"{color}{t}{colors.reset}" for t in text])
