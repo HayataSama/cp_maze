1d9986fd5393b5aaca5788eccff92e372f39278f
HayataSama <amir.khateri135@gmail.com>
2024-01-28 01:28:41 +0330

    fixed path finding fixed small issues refactor code

--- maze/main.py
+++ maze/main.py
@@ -13,12 +13,11 @@ from mazebackend import generate_maze
 # get maze width and maze height from the user
 WIDTH = int(input("Enter an odd number for Width: "))
 HEIGHT = int(input("Enter an odd number for Height: "))
-START = []
-END = []
-value_matrix = np.zeros((HEIGHT, WIDTH))
-junctions = []
 assert WIDTH % 2 == 1 and WIDTH >= 3
 assert HEIGHT % 2 == 1 and HEIGHT >= 3
+history = []  # list of moves that are performed in each path
+correct_paths = []  # list of paths that reach the end
+dead_ends = []  # list of paths that reach a dead-end
 
 
 ########################
@@ -38,25 +37,26 @@ def printc(*text, color="\033[0m", sep=" ", end="\n"):
     print(*[f"{color}{t}{colors.reset}" for t in text], end=end, sep=sep)
 
 
-def get_maze():
-    global START, END, value_matrix
+def get_maze(maze):
+    value_matrix = np.copy(maze)
     # find start and end position in the matrix
-    START = (
+    start = (
         int(np.where(value_matrix == 2)[0][0]),
         int(np.where(value_matrix == 2)[1][0]),
     )
-    END = (
+    end = (
         int(np.where(value_matrix == -2)[0][0]),
         int(np.where(value_matrix == -2)[1][0]),
     )
-    printc(f"start: {START},    end: {END}")
-
+    history.append(start)
     # assign value to each cell base on its distance to the end point
     for i, val in np.ndenumerate(value_matrix):
-        if val == 1:
-            # value_matrix[i] = ((i[0] - END[0]) ** 2) + ((i[1] - END[1]) ** 2)
-            value_matrix[i] = abs(i[0] - END[0]) + abs(i[1] - END[1])
-    printc(value_matrix)
+        if val != 0:
+            # value_matrix[i] = ((i[0] - end[0]) ** 2) + ((i[1] - end[1]) ** 2)
+            value_matrix[i] = abs(i[0] - end[0]) + abs(i[1] - end[1])
+
+    printc(f"\ncell values:\n{value_matrix}")
+    return start, end, value_matrix
 
 
 def find_move(position):
@@ -71,33 +71,48 @@ def find_move(position):
     # find all possible moves for a given position
     possible_moves = []
     for pos in neighbor_cells:
-        if maze[pos] != 0:
+        if (MAZE[pos] == 1 or MAZE[pos] == -2) and pos != history[-1]:
             possible_moves.append(pos)
 
-    printc("init position: ", position, neighbor_cells, possible_moves)
+    return possible_moves
 
-    # move the head
-    # TODO: tidyup this section
-    if len(possible_moves) == 1:
-        position = possible_moves[0]
-    elif len(possible_moves) > 1:
-        # in case of multiple possible moves, choose the cell that is closer to the end point
-        min_val = 100000  # should be greater than the maximum disctance possible from end point
-        min_val_pos = []
-        for i in possible_moves:
-            if value_matrix[i] < min_val:
-                min_val = value_matrix[i]
-                min_val_pos = i
-        position = min_val_pos
-        possible_moves.remove(min_val_pos)
-        junctions.append(
-            possible_moves
-        )  # junctions are cells that multiple moves can be performed from them
 
-    printc("final position: ", position, neighbor_cells, possible_moves)
-    # TODO: define a history variable that contains all moves preformed. then you can copy history from beginning till history.index(junctions[0][i])
-    # TODO: make this function recursive. check to see if we reached the end point or not. if we reached it we can append the path that we took to a list
-    # TODO: and try other paths. (junctions)
+def move(position, possible_moves):
+    # if at a given point, multiple moves can be performed, choose the one that gets the head closer to the end point first
+    min_val = (
+        100000  # should be greater than the maximum distance possible from end point
+    )
+    for i in possible_moves:
+        if VALUE_MATRIX[i] < min_val:
+            min_val = VALUE_MATRIX[i]
+            min_val_pos = i
+    history.append(position)
+    position = min_val_pos
+    return position
+
+
+def find_path(position):
+    # finds a path, it can lead to the end point or a dead-end
+    possible_moves = find_move(position)
+    history.pop()  # this makes sure the START position is not duplicated
+    while len(possible_moves) != 0:
+        position = move(position, possible_moves)
+        possible_moves = find_move(position)
+        # check whether we have reached the end or a dead-end
+        if position == END:
+            history.append(position)  # history does not contain the current position
+            correct_paths.append(history)
+            printc("reached the end!")
+            printc(f"path: {history}")
+            history.clear()
+            break
+        if position != END and len(possible_moves) == 0:
+            history.append(position)  # history does not contain the current position
+            dead_ends.append(history)
+            printc("reached a dead-end")
+            printc(f"path: {history}")
+            history.clear()
+            break
 
 
 ##################
@@ -106,10 +121,10 @@ def find_move(position):
 # generate maze
 # random.seed(4)
 random.seed(int(random.random() * 10))
-maze = generate_maze(HEIGHT, WIDTH)  # generated maze is a numpy ndarray
-value_matrix = np.copy(maze)
-printc(maze)
+MAZE = generate_maze(HEIGHT, WIDTH)  # generated maze is a numpy ndarray
+printc(f"\nmaze:\n{MAZE}")
 
+START, END, VALUE_MATRIX = get_maze(MAZE)
+find_path(START)
 
-get_maze()
-find_move(START)
+# TODO: define junctions variable to find other paths. it should probably be a dictionary so each junction's relationship (parent, child, sibling) with other junctions is specified.
